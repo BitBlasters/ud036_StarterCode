c3c0fb7b4f20566d93309e7baf38d2f88ce94dc6
Scott Mitchell <scottmitchell@mac.com>
2017-08-06 12:30:19 -0600

    Mode more changes to formatting of code

--- media.py
+++ media.py
@@ -1,7 +1,11 @@
 # Movie class and constructor
 class Movie():
-    """ The Movie class creates an instance of a moview object to show
-    in a web page"""
+    """ The Movie class creates an instance of a movie to show
+    in a web page. In the webs page for each movie it will show
+    the moview Title, Poster Image or other image for the movie,
+    and when the user clicks on a movie the user will be presented
+    with trailer for the movie for the user to watch"""
+    
     # init method to create the movie object
     def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
         """ inits the class with movie_title, poster_image_url,
